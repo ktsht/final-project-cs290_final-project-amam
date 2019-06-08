77f95c1ced20ee4dc419f5c367932d1ada502501
Aishwarya <vellanka@oregonstate.edu>
2019-06-07 17:10:15 -0700

    Moved files to public. Signup function is storing data in DB.

---
+++ public/signup.js
@@ -0,0 +1,48 @@
+var button = document.getElementsByClassName('sign-up-button')[0];
+
+button.addEventListener('click', function (event){
+    var firstNameInput = document.getElementById('first-name-input').value;
+    var lastNameInput = document.getElementById('last-name-input').value;
+    var majorInput = document.getElementById('major-input').value;
+    var yearOption = document.getElementById('dropdown-content').value;
+    console.log("yearOption:", yearOption);
+    var usernameInput = document.getElementById('username-input').value;
+    var passwordInput = document.getElementById('password-input').value;
+    if (!(firstNameInput || firstNameInput.value)){
+        alert("Please fill all required fields.")
+    }
+    else if (!(firstNameInput || firstNameInput.value)){
+        alert("Please fill all required fields.")
+    }
+    else if (!(lastNameInput || lastNameInput.value)){
+        alert("Please fill all required fields.")
+    }
+    else if (!(majorInput || majorInput.value)){
+        alert("Please fill all required fields.")
+    }
+    else if (yearOption === "--Select--"){
+        console.log("inside this if statement");
+        alert("Please select a year from the drop down list.")
+    }
+    else if (!(usernameInput || usernameInput.value)){
+        alert("Please fill all required fields.")
+    }
+    else if (!(passwordInput || passwordInput.value)){
+        alert("Please fill all required fields.")
+    }else{
+        var postRequest = new XMLHttpRequest();
+        var requestURL = '/signup';
+        postRequest.open('POST', requestURL);
+
+        var requestBody = JSON.stringify({
+            first_name: firstNameInput,
+            last_name: lastNameInput,
+            major: majorInput,
+            year: yearOption,
+            username: usernameInput,
+            password: passwordInput
+        });
+    }
+    postRequest.setRequestHeader('Content-Type', 'application/json');
+    postRequest.send(requestBody);
+});
