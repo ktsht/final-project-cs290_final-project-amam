252fcc38cbd4067e2b92429edcf5532d6df790fa
Aishwarya <vellanka@oregonstate.edu>
2019-06-09 17:37:45 -0700

    update style.css newpost.css main.handlebars for fonts, and update signup files

--- signup.js
+++ signup.js
@@ -5,9 +5,9 @@ button.addEventListener('click', function (event){
     var lastNameInput = document.getElementById('last-name-input').value;
     var majorInput = document.getElementById('major-input').value;
     var yearOption = document.getElementById('dropdown-content').value;
-    console.log("yearOption:", yearOption);
     var usernameInput = document.getElementById('username-input').value;
     var passwordInput = document.getElementById('password-input').value;
+    var profileInput = document.getElementById('profile-input').value;
     if (!(firstNameInput || firstNameInput.value)){
         alert("Please fill all required fields.")
     }
@@ -30,4 +30,31 @@ button.addEventListener('click', function (event){
     else if (!(passwordInput || passwordInput.value)){
         alert("Please fill all required fields.")
     }
+    else if (!(profileInput || profileInput.value)){
+            alert("Please fill all required fields.")
+    }else{
+        var postRequest = new XMLHttpRequest();
+        var requestURL = '/signup';
+        postRequest.open('POST', requestURL);
+
+        var requestBody = JSON.stringify({
+            first_name: firstNameInput,
+            last_name: lastNameInput,
+            major: majorInput,
+            year: yearOption,
+            username: usernameInput,
+            password: passwordInput,
+            profile_pic: profileInput
+        });
+    }
+
+    postRequest.setRequestHeader('Content-Type', 'application/json');
+    postRequest.send(requestBody);
+    document.getElementById('first-name-input').value = "";
+    document.getElementById('last-name-input').value = "";
+    document.getElementById('major-input').value = "";
+    document.getElementById('year-input').value = "--Select--";
+    document.getElementById('username-input').value = "";
+    document.getElementById('password-input').value = "";
+    document.getElementById('profile-input').value = "";
 });
